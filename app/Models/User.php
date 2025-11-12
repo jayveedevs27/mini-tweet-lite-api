5035ca4e386c57f9f6790c9652108dab639e22ce
DESKTOP-9Q9S1KN\jayve <jayvee.salango10@gmail.com>
2025-11-12 21:12:50 +0800

    tests: added HasFactory for using feature test

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
+    use HasApiTokens, Notifiable, HasFactory;
 
     protected $fillable = [
         'first_name',
